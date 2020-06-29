a625f04ea6bac7eeaa38b5ddea178e56758ede16
Duc <vanduc2514@gmail.com>
2020-06-30 01:49:18 +0700

    Exception TS2322: Type '{}' is not assignable to type 'IPost'. Exception TS2322: Type '50' is not assignable to type 'string'.

--- src/fundamentals.ts
+++ src/fundamentals.ts
@@ -98,11 +98,40 @@ function main() {
  */
 
 function main() {
-    console.log(x);
-    var  x = 5;
+    let message: string;
+    let total: number = 100;
+    let isProduction = true;
+    let prices: Array<number> = [120, 88, 60];
+    let languages: String[] = ["vi", "en-us"];
+    let now = new Date();
+    let unknown: any;
 
-    console.log(y);
-    let y = 10;
+    enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    function log(msg: string): void {
+        console.log(msg);
+    }
+
+    interface IPost {
+        id: string;
+        title: string;
+        body?: string;
+    }
+
+    isProduction = false;
+    unknown = Direction.UP;
+    unknown = "changed";
+
+    const post: IPost = {
+
+    };
+    message = 50;
+
 }
 
 main();
