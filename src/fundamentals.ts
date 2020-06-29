e17712018acefdd1d02bfae5169487b8d08aa027
Duc <vanduc2514@gmail.com>
2020-06-30 01:37:07 +0700

    Exception TS2034: Cannot find name 'target'

--- src/fundamentals.ts
+++ src/fundamentals.ts
@@ -96,8 +96,16 @@ function main() {
 }
  */
 
-function main() {}
-
-// tslint:enable
+function main() {
+    console.log("START");
+    if (true) {
+        var lang = "vi";
+        let target = "en-us";
+        console.log("inside block");
+        console.log(target);
+    }
+    console.log(lang);
+    console.log(target);
+}
 
 main();
