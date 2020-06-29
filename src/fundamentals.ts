89ae1849b19e048e6dae17ee242dcae5fe5d72c7
Duc <vanduc2514@gmail.com>
2020-06-30 01:41:39 +0700

    Exception TS2448: Block-scoped variable 'y' used before its declaration.

--- src/fundamentals.ts
+++ src/fundamentals.ts
@@ -98,15 +98,11 @@ function main() {
  */
 
 function main() {
-    var x = 5;
     console.log(x);
-    var x = 10;
-    console.log(x);
+    var  x = 5;
 
-    let y = 55;
     console.log(y);
-    let y = 100;
-    console.log(y);
+    let y = 10;
 }
 
 main();
