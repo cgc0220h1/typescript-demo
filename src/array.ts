46c8a0d98228fc1773fe0772b4735373b8c07e33
Duc <vanduc2514@gmail.com>
2020-06-30 21:11:40 +0700

    Add function

--- src/array.ts
+++ src/array.ts
@@ -83,5 +83,5 @@ console.log(x[0].substr(1));
 // console.log(x[1].substr(1));
 x[3] = "world";
 x[5] = 100.25;
-console.log(x[5].toString)
+console.log(x[5].toString())
 // x[6] = true;
--- src/functions.ts
+++ src/functions.ts
@@ -1,25 +1,47 @@
 import './scss/styles.scss';
 
-function square(num: number): number {
-  return num * num;
-}
+// function square(num: number): number {
+//   return num * num;
+// }
 
-console.log(square(5));
+// console.log(square(5));
 // Output: 25
 
 // Function expression
-const squareFE = function (num: number): number {
-  return num * num;
-}
+// const squareFE = function (num: number): number {
+//   return num * num;
+// }
 
 // Higher-order function
+// function add(a: number): Function {
+//   return function(b: number): number {
+//     return a + b;
+//   }
+// }
+
+// const addWith5 = add(5);
+// console.log(addWith5(3));
+// console.log(addWith5(15));
+
+function square(num: number): number {
+    return num * num;
+}
+
+console.log(square(5));
+// console.log(square("a"));
+
+const squareFE = function (num: number): number {
+    return num * num;
+}
+console.log(squareFE(9));
+
 function add(a: number): Function {
-  return function(b: number): number {
-    return a + b;
-  }
+    return function (b: number): number {
+        return a + b;
+    }
 }
 
 const addWith5 = add(5);
+console.log(addWith5);
 console.log(addWith5(3));
 console.log(addWith5(15));
-
